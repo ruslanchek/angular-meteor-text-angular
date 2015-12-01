e591c4dfaaafb18413c25b947fd651696453626a
Ruslan Shashkov <rshashkov@icloud.com>
2015-12-01 08:48:48 +0200

    Package fix

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 // package metadata file for Meteor.js
 var packageName = 'rshashkov:angular-meteor-text-angular';
 var where = 'client'; // where to install: 'client' or 'server'. For both, pass nothing.
-var version = '0.0.1';
+var version = '0.0.2';
 var summary = 'Text editor for Angular';
 var gitLink = 'https://github.com/ruslanchek/angular-meteor-text-angular';
 var documentationFile = 'README.md';
@@ -17,7 +17,7 @@ Package.describe({
 
 Package.onUse(function(api) {
 	api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.1']); // Meteor versions
+	api.addFiles('src/textAngularSetup.js', where); // Files in use
 	api.addFiles('src/textAngular.js', where); // Files in use
 	api.addFiles('src/textAngular-sanitize.js', where); // Files in use
-	api.addFiles('src/textAngularSetup.js', where); // Files in use
 });
